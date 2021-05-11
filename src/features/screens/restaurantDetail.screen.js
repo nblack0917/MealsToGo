2477cc837f07470f3f3ac79665766b621ec7f42a
nblack0917 <nickablack@gmail.com>
2021-05-11 09:04:34 -0500

    added accordian groups

--- src/features/screens/restaurantDetail.screen.js
+++ src/features/screens/restaurantDetail.screen.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { SafeViewContainer } from "../../utils/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { MenuCard } from '../components/restaurant-menu-componet'
 
 export const RestaurantDetailScreen = ({ route, navigation }) => {
   const { restaurant } = route.params;
 
   return (
-    <View>
+    <SafeViewContainer>
       <RestaurantInfoCard restaurant={restaurant} />
-      
-    </View>
+      <MenuCard />
+    </SafeViewContainer>
   );
 };
